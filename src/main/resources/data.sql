d0037556aa39ca61c113b06ba63be8adc6c8aafe
joaomarcelo-franca <joao.franca@al.infnet.edu.br>
2026-04-16 22:04:33 -0300

    populando banco com .sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,14 @@
+INSERT INTO aventura.aventureiros (organizacao_id, usuario_id, companheiro_id, nome, classe, nivel, ativo, criado_em, atualizado_em)
+VALUES (1, 1, NULL, 'Arthos', 'GUERREIRO', 10, TRUE, NOW(), NOW());
+
+INSERT INTO aventura.aventureiros (organizacao_id, usuario_id, companheiro_id, nome, classe, nivel, ativo, criado_em, atualizado_em)
+VALUES (2, 2, NULL, 'Lyra', 'MAGO', 8, TRUE, NOW(), NOW());
+
+INSERT INTO aventura.aventureiros (organizacao_id, usuario_id, companheiro_id, nome, classe, nivel, ativo, criado_em, atualizado_em)
+VALUES (1, 3, NULL, 'Kael', 'ARQUEIRO', 12, TRUE, NOW(), NOW());
+
+INSERT INTO aventura.aventureiros (organizacao_id, usuario_id, companheiro_id, nome, classe, nivel, ativo, criado_em, atualizado_em)
+VALUES (2, 1, NULL, 'Selene', 'CLERIGO', 7, TRUE, NOW(), NOW());
+
+INSERT INTO aventura.aventureiros (organizacao_id, usuario_id, companheiro_id, nome, classe, nivel, ativo, criado_em, atualizado_em)
+VALUES (1, 2, NULL, 'Darius', 'LADINO', 9, TRUE, NOW(), NOW());
